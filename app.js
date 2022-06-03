4bf09d75991b7513f52083424a81c43d4289405f
JSVenger <peteryar19@gmail.com>
2022-06-03 23:33:19 +0100

    Basic socket app implemented

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
 const express = require('express');
 const http = require('http');
 const app = express();
+const { Server, Socket } = require('socket.io');
 
 const server = http.createServer(app)
+const io = new Server(server)
 
-app.get('/', (req, res)=>{
-   res.sendFile(__dirname+'/index.html');
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
 })
 
-server.listen(3000, ()=>console.log('Listening in PORT 3000....'))
+io.on('connect', (socket) => {
+    console.log('A user connected');
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+})
+io.on('connection', (socket) => {
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg);
+      console.log('message: ' + msg);
+    });
+  });
+server.listen(3000, () => console.log('Listening in PORT 3000....'))
